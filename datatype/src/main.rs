3756ce58af3077564cd055ad9633734bd51c1287
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-02-08 18:21:29 +0800

    value

--- datatype/src/main.rs
+++ datatype/src/main.rs
@@ -1,13 +1,48 @@
-fn main() {
-    println!("Hello, world!");
 
-    let x: bool = true;
-    let y: bool = true;
-    let x = 5;
-    if x > 1 {
-        println!("x is bigger than 1");
-    }
-    // as操作符 将bool类型转成数字 0 和 1 
-    assert_eq!(x as i32, 1);
-    assert_eq!(y as i32, 1);
+fn is_copy<T: Copy>() {}
+
+fn types_impl_copy_trait() {
+    is_copy::<bool>();
+    is_copy::<char>();
+
+    // all iXX and uXX, usize/isize, fXX implement Copy trait
+    is_copy::<i8>();
+    is_copy::<u64>();
+    is_copy::<i64>();
+    is_copy::<usize>();
+
+    // function (actually a pointer) is Copy
+    is_copy::<fn()>();
+
+    // raw pointer is Copy
+    is_copy::<*const String>();
+    is_copy::<*mut String>();
+
+    // immutable reference is Copy
+    is_copy::<&[Vec<u8>]>();
+    is_copy::<&String>();
+
+    // array/tuple with values which is Copy is Copy
+    is_copy::<[u8; 4]>();
+    is_copy::<(&str, &str)>();
+}
+
+fn types_not_impl_copy_trait() {
+    // unsized or dynamic sized type is not Copy
+    is_copy::<str>();
+    is_copy::<[u8]>();
+    is_copy::<Vec<u8>>();
+    is_copy::<String>();
+
+    // mutable reference is not Copy
+    is_copy::<&mut String>();
+
+    // array / tuple with values that not Copy is not Copy
+    is_copy::<[Vec<u8>; 4]>();
+    is_copy::<(String, u32)>();
+}
+
+fn main() {
+    types_impl_copy_trait();
+    types_not_impl_copy_trait();
 }
