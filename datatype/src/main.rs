34977f60cac7086247c841875ec8175dc418a204
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-02-09 22:51:00 +0800

    value

--- datatype/src/main.rs
+++ datatype/src/main.rs
@@ -1,48 +1,41 @@
-
-fn is_copy<T: Copy>() {}
-
-fn types_impl_copy_trait() {
-    is_copy::<bool>();
-    is_copy::<char>();
-
-    // all iXX and uXX, usize/isize, fXX implement Copy trait
-    is_copy::<i8>();
-    is_copy::<u64>();
-    is_copy::<i64>();
-    is_copy::<usize>();
-
-    // function (actually a pointer) is Copy
-    is_copy::<fn()>();
-
-    // raw pointer is Copy
-    is_copy::<*const String>();
-    is_copy::<*mut String>();
-
-    // immutable reference is Copy
-    is_copy::<&[Vec<u8>]>();
-    is_copy::<&String>();
-
-    // array/tuple with values which is Copy is Copy
-    is_copy::<[u8; 4]>();
-    is_copy::<(&str, &str)>();
-}
-
-fn types_not_impl_copy_trait() {
-    // unsized or dynamic sized type is not Copy
-    is_copy::<str>();
-    is_copy::<[u8]>();
-    is_copy::<Vec<u8>>();
-    is_copy::<String>();
-
-    // mutable reference is not Copy
-    is_copy::<&mut String>();
-
-    // array / tuple with values that not Copy is not Copy
-    is_copy::<[Vec<u8>; 4]>();
-    is_copy::<(String, u32)>();
-}
+use std::mem;
 
 fn main() {
-    types_impl_copy_trait();
-    types_not_impl_copy_trait();
+    // capacity 是 1, len 是 0
+    let mut v = vec![1];
+    // capacity 是 8, len 是 0
+    let v1: Vec<i32> = Vec::with_capacity(8);
+
+    print_vec("v1", v1);
+
+    // 我们先打印 heap 地址，然后看看添加内容是否会导致堆重分配
+    println!("heap start: {:p}", &v[0] as *const i32);
+
+    extend_vec(&mut v);
+
+    // heap 地址改变了！这就是为什么可变引用和不可变引用不能共存的原因
+    println!("new heap start: {:p}", &v[0] as *const i32);
+
+    print_vec("v", v);
+}
+
+fn extend_vec(v: &mut Vec<i32>) {
+    // Vec<T> 堆内存里 T 的个数是指数增长的，我们让它恰好 push 33 个元素
+    // capacity 会变成 64
+    (2..34).into_iter().for_each(|i| v.push(i));
+}
+
+fn print_vec<T>(name: &str, data: Vec<T>) {
+    let p: [usize; 3] = unsafe { mem::transmute(data) };
+    // 打印 Vec<T> 的堆地址，capacity，len
+    println!("{}: 0x{:x}, {}, {}", name, p[0], p[1], p[2]);
+}
+
+fn test() {
+    let mut arr = vec![1, 2, 3];
+    // cache the last item
+    let last = arr.last();
+    arr.push(4);
+    // consume previously stored last item
+    println!("last: {:?}", last);
 }
