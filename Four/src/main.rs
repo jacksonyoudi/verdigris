6a9d03908acbb7c5afc6e91969e1158c196a084a
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-07-31 19:00:28 +0800

    closure

--- Four/src/main.rs
+++ Four/src/main.rs
@@ -31,6 +31,36 @@ fn main() {
     let sum = |y| x + y;
 
     assert_eq!(3, sum(2));
+
+
+    struct Cacher<T>
+        where T: Fn(u32) -> u32 {
+        query: T,
+        value: Option<u32>,
+    }
+
+    impl<T> Cacher<T>
+        where
+            T: Fn(u32) -> u32 {
+        fn new(query: T) -> Cacher<T> {
+            Cacher {
+                query,
+                value: None,
+            }
+        }
+
+        // 先查询缓存值 `self.value`，若不存在，则调用 `query` 加载
+        fn value(&mut self, args: u32) -> u32 {
+            match self.value {
+                Some(v) => v,
+                None => {
+                    let v = (self.query)(args);
+                    self.value = Some(v);
+                    v
+                }
+            }
+        }
+    }
 }
 
 
