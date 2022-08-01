4b478287981d26cee4b15d7d8b8b6dcd4a86b2ca
jacksonyoudi <liangchangyoujackson@gmail.com>
2022-08-01 22:32:28 +0800

    cache

--- Four/src/main.rs
+++ Four/src/main.rs
@@ -1,4 +1,7 @@
+mod cache;
+
 use std::{thread, time};
+
 
 fn main() {
     // String是
@@ -33,34 +36,50 @@ fn main() {
     assert_eq!(3, sum(2));
 
 
-    struct Cacher<T>
-        where T: Fn(u32) -> u32 {
-        query: T,
-        value: Option<u32>,
-    }
+    // struct Cacher<T>
+    //     where T: Fn(u32) -> u32 {
+    //     query: T,
+    //     value: Option<u32>,
+    // }
+    //
+    // impl<T> Cacher<T>
+    //     where
+    //         T: Fn(u32) -> u32 {
+    //     fn new(query: T) -> Cacher<T> {
+    //         Cacher {
+    //             query,
+    //             value: None,
+    //         }
+    //     }
+    //
+    //     // 先查询缓存值 `self.value`，若不存在，则调用 `query` 加载
+    //     fn value(&mut self, args: u32) -> u32 {
+    //         match self.value {
+    //             Some(v) => v,
+    //             None => {
+    //                 let v = (self.query)(args);
+    //                 self.value = Some(v);
+    //                 v
+    //             }
+    //         }
+    //     }
+    // }
 
-    impl<T> Cacher<T>
-        where
-            T: Fn(u32) -> u32 {
-        fn new(query: T) -> Cacher<T> {
-            Cacher {
-                query,
-                value: None,
-            }
-        }
+    let x = 4;
+    let equal_to_x = |z| z == x;
+    let y = 4;
+    assert!(equal_to_x(y));
 
-        // 先查询缓存值 `self.value`，若不存在，则调用 `query` 加载
-        fn value(&mut self, args: u32) -> u32 {
-            match self.value {
-                Some(v) => v,
-                None => {
-                    let v = (self.query)(args);
-                    self.value = Some(v);
-                    v
-                }
-            }
-        }
-    }
+    let x = vec![1, 2, 3, 4];
+    fn_once(|z| { z == x.len() })
+}
+
+
+fn fn_once<F>(func: F)
+    where F: FnOnce(usize) -> bool + Copy,
+{
+    println!("{}", func(3));
+    println!("{}", func(4))
 }
 
 
